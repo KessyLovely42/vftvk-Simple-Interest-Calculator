3ce80924d9d6f2a2c3b98bd51237237465b54426
Kessy Lovely <kessylovely42@gmail.com>
2022-04-04 19:46:37 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    //convert the years value to future year.
+    var year = new Date().getFullYear() + parseInt(years);
+
+    interest = principal * rate / years;
+    document.getElementById("result").innerHTML = "If you deposit " + principal + "<br>"
+        + "at an interest rate of " + rate +
+        "<br>" + " You will receive an amount of " + interest + "<br>" +
+        "in the year " + year;
+}
+
+//function to show slider value
+function slider() {
+    document.getElementById("rate_value").innerText = document.getElementById("rate").value;
 }
         
